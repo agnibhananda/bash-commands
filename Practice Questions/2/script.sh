bc8e1255555eab2997e3d7e4c58ddfd7d7b4103d
Agnibha Nanda <agnibhananda@gmail.com>
2025-08-16 15:02:14 +0530

    practice questions

---
+++ Practice Questions/2/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+total_bill=0
+
+while IFS= read -r line; do
+    if [[ "$line" == *"<li>Item"* ]]; then
+        price=$(echo "$line" | grep -oP '(?<=<span class="item-price">Rs\. )\d+(?=</span>)')
+        qty=$(echo "$line" | grep -oP '(?<=<span class="item-qty">)\d+(?=</span>)')
+        total_bill=$((total_bill + price * qty))
+    fi
+done
+
+echo "$total_bill"
+
+
+#use \d+ to capture number and .*? to capture any character in between
